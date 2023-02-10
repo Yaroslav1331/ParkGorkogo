78a3c1bc82a4ff949112f3832e7a02eeb93c0245
Yaroslav1331 <85566910+Yaroslav1331@users.noreply.github.com>
2023-02-10 18:44:19 +0300

    Add files via upload

--- AddOrderForm.cs
+++ AddOrderForm.cs
@@ -26,7 +26,7 @@ namespace SantoGost
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
             DataTable table = new DataTable();
             
-            string querystring = $"INSERT INTO Order_Table(Code_Order, Date_Order_Open, Time_Order, Code_Client, Service_Id, Status_Id, Date_Order_Close, Time_Rent) VALUES ({textBox8.Text}, {textBox1.Text}, {textBox2.Text}, {textBox3.Text}, {textBox4.Text}, {textBox5.Text}, {textBox6.Text}, {textBox7.Text})";
+            string querystring = $"INSERT INTO Order_Table(Code_Order, Date_Order_Open, Time_Order, Code_Client, Service_Id, Status_Id, Date_Order_Close, Time_Rent) VALUES ({textBox1.Text}, {textBox2.Text}, {textBox3.Text}, {textBox4.Text}, {textBox5.Text}, {textBox6.Text}, {textBox7.Text})";
             SqlCommand command = new SqlCommand(querystring, database.GetConnection());
 
             database.closeConnection();
